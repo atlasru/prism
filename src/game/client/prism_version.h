dcb7a07c2dc761350358a6b3d89683e793a83277
Yan <258285264+atlasru@users.noreply.github.com>
2026-09-25 20:56:09 +0300

    release: finalize Prism v0.2.0 version

--- src/game/client/prism_version.h
+++ src/game/client/prism_version.h
@@ -1,5 +1,5 @@
 // Prism additions, distributed under the zlib license in license.txt.
 #ifndef GAME_CLIENT_PRISM_VERSION_H
 #define GAME_CLIENT_PRISM_VERSION_H
-#define PRISM_VERSION "0.2.0-dev"
+#define PRISM_VERSION "0.2.0"
 #endif
